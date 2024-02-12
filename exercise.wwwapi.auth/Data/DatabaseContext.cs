f04e92bae4b67466e4ea4dc0be8e6554994e8405
llllllll-l <vincent.werelius@gmail.com>
2024-02-12 12:01:43 +0100

    step 2.6

--- exercise.wwwapi.auth/Data/DatabaseContext.cs
+++ exercise.wwwapi.auth/Data/DatabaseContext.cs
@@ -1,6 +1,26 @@
-﻿namespace exercise.wwwapi.auth.Data
+﻿using exercise.wwwapi.auth.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace exercise.wwwapi.auth.Data
 {
-    public class DbContext
+    public class DatabaseContext : IdentityUserContext<ApplicationUser>
     {
+        private readonly string _connectionstring;
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) {
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            _connectionstring = configuration.GetValue<string>("ConnectionStrings:DefaultConnectionString")!;
+            this.Database.EnsureCreated();
+        }
+
+        public override void OnModelCreating(ModelBuilder modelBuilder) {
+
+
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
     }
 }
--- exercise.wwwapi.auth/Program.cs
+++ exercise.wwwapi.auth/Program.cs
@@ -1,3 +1,10 @@
+using System.Text;
+using exercise.wwwapi.auth.Data;
+using exercise.wwwapi.auth.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +14,37 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => {
+    options.SignIn.RequireConfirmedAccount = false;
+    options.User.RequireUniqueEmail = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequireDigit = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = false;
+}).AddRoles<IdentityRole>().AddEntityFrameworkStores<DatabaseContext>();
+
+var validIssuer = builder.Configuration.GetValue<string>("JwtTokenSettings:ValidIssuer");
+var validAudience = builder.Configuration.GetValue<string>("JwtTokenSettings:ValidAudience");
+
+var symmetricSecurityKey = builder.Configuration.GetValue<string>("JwtTokenSettings:SymmetricSecurityKey");
+
+builder.Services.AddAuthentication();
+builder.Services.AddAuthentication(options => {
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options => {
+    options.IncludeErrorDetails = true;
+    options.TokenValidationParameters = new TokenValidationParameters() {
+        ClockSkew = TimeSpan.Zero,
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = validIssuer,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(symmetricSecurityKey)),
+    };
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,7 +56,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
 
 
 
