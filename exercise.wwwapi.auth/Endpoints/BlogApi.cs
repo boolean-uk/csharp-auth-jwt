90b6ee2caa6693ec5ce737b1d0d5130717bbe828
llllllll-l <vincent.werelius@gmail.com>
2024-02-12 16:47:40 +0100

    extensions

--- exercise.wwwapi.auth/Endpoints/BlogApi.cs
+++ exercise.wwwapi.auth/Endpoints/BlogApi.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Http;
+﻿using exercise.wwwapi.auth.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Web.Http;
+using static exercise.wwwapi.auth.Payloads.BlogPayloads;
 
 namespace exercise.wwwapi.auth.Endpoints
 {
@@ -9,14 +12,21 @@ namespace exercise.wwwapi.auth.Endpoints
         {
             var blogGroup = app.MapGroup("/blog");
             blogGroup.MapPost("/", CreateBlog);
+            blogGroup.MapPost("/", UpdateBlog);
         }
 
 
         [Authorize()]
-        public static async Task<IResult> CreateBlog()
+        public static async Task<IResult> CreateBlog(UserManager<ApplicationUser> userManager, BlogPostPayload payload)
         {
-            
+            var currentUser = await userManager.GetUserAsync(HttpContext.User); // <-- may work
 
+
+            throw new NotImplementedException();
+        }
+
+        public static async Task<IResult> UpdateBlog()
+        {
 
             throw new NotImplementedException();
         }
