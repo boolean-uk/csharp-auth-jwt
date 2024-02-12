634eaf279aa7dec5f0967057662c24c059fa4aaa
llllllll-l <vincent.werelius@gmail.com>
2024-02-12 12:10:20 +0100

    ApplicationUser table

--- exercise.wwwapi.auth/Models/ApplicationUser.cs
+++ exercise.wwwapi.auth/Models/ApplicationUser.cs
@@ -1,11 +1,20 @@
-﻿using exercise.wwwapi.auth.Enums;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using exercise.wwwapi.auth.Enums;
 using Microsoft.AspNetCore.Identity;
 
 namespace exercise.wwwapi.auth.Models
 {
+    [Table("users")]
     public class ApplicationUser : IdentityUser
     {
-
+        [Column("first_name")]
+        public string FirstName { get; set; }
+        [Column("last_name")]
+        public string LastName { get; set; }
+        [Column("email")]
+        public string Email { get; set; }
+        public UserRole Role { get; set; }
+        [Column("role")]
         public UserRole Role { get; set; }
     }
 }
