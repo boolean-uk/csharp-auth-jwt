b4dca5f918ad67d4315ecea32aeb40af98d9327e
Sini Suihkonen <sahsuihkonen@gmail.com>
2024-02-12 17:43:14 +0200

    core + extensions

---
+++ BlogApplication/Repository/IBlogRepository.cs
@@ -0,0 +1,17 @@
+﻿using BlogApplication.Models;
+
+namespace BlogApplication.Repository
+{
+    public interface IBlogRepository
+    {
+        public List<BlogPost> GetAllBlogPosts(string id, string role, string email);
+
+        public BlogPost? GetBlogPost(string id);
+
+        public BlogPost AddBlogPost(string text, string authId, string email);
+
+        public BlogPost? UpdateBlogPost(string id, BlogPostUpdatePayload updateData);
+
+        public ApplicationUser? GetUser(string email);
+    }
+}
