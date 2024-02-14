5b7bf8236497f53d0c2742309455c65a9451f0d3
Benjamin Samuelsen Austestad <ben_7aus@hotmail.com>
2024-02-14 16:01:40 +0100

    Start implementation of application layer

---
+++ exercise.Application/ServiceResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise.Application
+{
+    public class ServiceResponse<T>
+    {
+        public T? Data { get; set; }
+        public bool Success = true;
+        public string Message = "";
+    }
+}
---
+++ exercise.Application/UserService.cs
@@ -0,0 +1,29 @@
+﻿using exercise.Data.Models;
+using exercise.Infrastructure;
+
+namespace exercise.Application
+{
+    public class UserService
+    {
+        private readonly IRepository<User> _repository;
+        public UserService(IRepository<User> repository) 
+        {
+            _repository = repository;
+        }
+
+        public async Task<ServiceResponse<User>> Get(string id)
+        {
+            ServiceResponse<User> response = new();
+            try
+            {
+                User user = await _repository.Get(id);
+                response.Data = user;
+            } catch (ArgumentException ex) 
+            {
+                response.Success = false;
+                response.Message = ex.Message;
+            }
+            return response;
+        }
+    }
+}
