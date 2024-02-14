91a2c2d5695c99032ca7ee802b846f2afb1e7401
Benjamin Samuelsen Austestad <ben_7aus@hotmail.com>
2024-02-14 15:43:56 +0100

    Implement infrastructure

--- exercise.wwwapi/Program.cs
+++ exercise.wwwapi/Program.cs
@@ -1,9 +1,16 @@
+using exercise.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
