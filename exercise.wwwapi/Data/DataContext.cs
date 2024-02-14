f5de2ee4b5b710c1a941fe9d6d51bed68ad32c21
oysteinbjo <oystein.bjorkeng@no.experis.com>
2024-02-14 15:09:45 +0100

    add some models, initial auth configuration

---
+++ exercise.wwwapi/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using exercise.wwwapi.DataModels;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace exercise.wwwapi.Data
+{
+    public class DataContext : IdentityDbContext<ApplicationUser>
+    {
+        public DataContext() 
+        {
+            
+        }
+
+        public DataContext(DbContextOptions options ) : base( options ) 
+        {
+            
+        }
+        
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        
+    }
+}
--- exercise.wwwapi/Program.cs
+++ exercise.wwwapi/Program.cs
@@ -1,9 +1,27 @@
+using exercise.wwwapi.Data;
+using exercise.wwwapi.DataModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options => 
+                                            options.UseNpgsql(builder.Configuration
+                                            .GetConnectionString("DefaultConnectionString")), 
+                                            ServiceLifetime.Scoped);
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+}).AddEntityFrameworkStores<DataContext>();
 
 var app = builder.Build();
 
@@ -15,6 +33,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 
 app.Run();
