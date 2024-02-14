f2202ef808bddc35e285d6f6027198ee0d7165b6
Stian Gaustad <stian.gaustad@no.experis.com>
2024-02-14 15:01:18 +0100

    Added db models

---
+++ exercise.wwwapi/Models/PureModels/Entry.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace exercise.wwwapi.Models.PureModels
+{
+    public class Entry
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string PostText { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set;}
+
+        public int AuthorId { get; set; }
+
+        public User Author { get; set; }
+    }
+}
---
+++ exercise.wwwapi/Models/PureModels/User.cs
@@ -0,0 +1,10 @@
+﻿using exercise.wwwapi.Enums;
+using Microsoft.AspNetCore.Identity;
+
+namespace exercise.wwwapi.Models.PureModels
+{
+    public class User : IdentityUser
+    {
+        public Role Role { get; set; }
+    }
+}
