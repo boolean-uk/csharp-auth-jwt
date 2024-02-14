432f6c06c42f5b983c6757a910e73a9430e283ac
Benjamin Samuelsen Austestad <ben_7aus@hotmail.com>
2024-02-14 15:01:19 +0100

    Implement data layer

---
+++ exercise.Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using exercise.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace exercise.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Posts)
+                .WithOne(p => p.User)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ exercise.Data/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace exercise.Data.Models
+{
+    [Table("users")]
+    public class User : IdentityUser, IEntity
+    {
+        [Column("posts")]
+        public List<Post> Posts { get; set; }
+    }
+}
