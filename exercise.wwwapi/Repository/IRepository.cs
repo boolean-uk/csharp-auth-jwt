c867997efee65039f062a41c0f115c2da77bc9b7
henrikrosenkilde <henrik.rosenkilde@hotmail.com>
2024-02-15 09:54:49 +0100

    Core + Extension💕

---
+++ exercise.wwwapi/Repository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace workshop.webapi.Repository
+{
+    public interface IRepository<T>
+    {
+        Task<IEnumerable<T>> Get();
+        Task<IEnumerable<T>> GetByAuthorId(string authorId);
+        Task<T> GetById(object id);
+        Task<T> Delete(T entity);
+        Task<T> Insert(T entity);
+        Task<T> Update(T entity);
+    }
+}
