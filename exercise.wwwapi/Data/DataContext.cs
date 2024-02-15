0e62bb06f0b27c2688ec6afdbd48f07e40459e39
Stian Gaustad <stian.gaustad@no.experis.com>
2024-02-15 11:10:34 +0100

    DataContext update

--- exercise.wwwapi/Data/DataContext.cs
+++ exercise.wwwapi/Data/DataContext.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using exercise.wwwapi.Data.Seed;
+using exercise.wwwapi.Models.PureModels;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace exercise.wwwapi.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityUserContext<ApplicationUser>
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+            Seeder seed = new Seeder();
+
+            modelBuilder.Entity<ApplicationUser>().HasData(seed.Users);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<ApplicationUser> Users { get; set; }
+        public DbSet<Entry> Entries { get; set; }
+
     }
 }
