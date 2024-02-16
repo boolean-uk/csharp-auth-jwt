a3bd82c0fbc21435dfaae1e33490d9b5bcd5e141
Oneallane <oneallane2808@gmail.com>
2024-02-16 14:06:54 +0100

    Core + extension complete

---
+++ exercise.wwwapi/DataTransfer/Request/RegistrationRequest.cs
@@ -0,0 +1,21 @@
+﻿using exercise.wwwapi.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace exercise.wwwapi.DataTransfer.Request
+{
+    public class RegistrationRequest
+    {
+
+        [Required]
+        public string? Email { get; set; }
+
+        [Required]
+        public string? Username { get { return this.Email; } set { } }
+
+        [Required]
+        public string? Password { get; set; }
+
+        public Role role { get; set; } = Role.User;
+
+    }
+}
---
+++ exercise.wwwapi/Repository/Repository.cs
@@ -0,0 +1,50 @@
+﻿
+using exercise.wwwapi.Data;
+using exercise.wwwapi.DataModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace exercise.wwwapi.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private DataContext _db;
+        private DbSet<T> _table;
+
+        public Repository(DataContext db)
+        {
+            _db = db;
+            _table = _db.Set<T>();
+        }
+
+        public async Task<T> CreateBlogPost(T entity)
+        {
+            _table.Add(entity);
+            await _db.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T> Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<T>> Get()
+        {
+            return await _table.ToListAsync();
+        }
+
+        public async Task<T> GetById(int id)
+        {
+
+            return await _db.Set<T>().FindAsync(id)
+                 ?? throw new ArgumentException($"Cant find entity of id: {id}");
+        }
+
+        public async Task<T> Update(T entity)
+        {
+            _table.Update(entity).State = EntityState.Modified;
+            await _db.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
