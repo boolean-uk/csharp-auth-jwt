0b4423f565f2837609fc4cde0b09f3ff409474ba
Aleksander Kolsrud <kols203@gmail.com>
2024-02-19 09:47:49 +0100

    Models, Db, repo set up

---
+++ exercise.wwwapi/Repo/Repo.cs
@@ -0,0 +1,49 @@
+﻿using exercise.wwwapi.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace exercise.wwwapi.Repo
+{
+    public class Repo<T> : Irepo<T> where T : class
+    {
+        private DataContext _db;
+        private DbSet<T> _table = null;
+
+        public Repo(DataContext context) 
+        {
+            _db = context;
+            _table = _db.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            return await _table.ToListAsync();
+        } 
+
+        public async Task<T> GetById(object id)
+        {
+            return await _table.FindAsync(id);
+        }
+
+        public async Task<T> Create(T entity)
+        {
+            _table.Add(entity);
+            await _db.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T> Update(T entity)
+        {
+            _table.Update(entity).State = EntityState.Modified;
+            await _db.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T> Delete(T entity)
+        {
+            _db.Entry(entity).State = EntityState.Deleted;
+            await _db.SaveChangesAsync();
+            return entity;
+        }
+  
+    }
+}
