af036138969072904ca038500a5949a9a347565e
Mikolaj <mkmbaran@gmail.com>
2024-02-20 11:55:36 +0100

    Fulfilled Auth Jwt exercise criteria

---
+++ exercise.wwwapi/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using exercise.wwwapi.DataModels;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace exercise.wwwapi.Data
+{
+    public class DataContext : IdentityUserContext<ApplicationUser>
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<BlogPost> Posts { get; set; }
+    }
+}
---
+++ exercise.wwwapi/DataModels/BlogPost.cs
@@ -0,0 +1,16 @@
+﻿using exercise.wwwapi.DataTransfers.Requests;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace exercise.wwwapi.DataModels
+{
+    [Table("blogs")]
+    public class BlogPost
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("text")]
+        public string Text { get; set; }
+        [Column("authorId")]
+        public string AuthorId { get; set; }
+    }
+}
