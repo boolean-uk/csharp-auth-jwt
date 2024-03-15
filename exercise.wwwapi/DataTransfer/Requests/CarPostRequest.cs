8f84b9d0f8ebeb156e5fadde68452825b3cb5cc3
Mateusz Pietrzak <matti3301@gmail.com>
2024-03-15 09:54:06 +0100

    Completed all tasks

---
+++ exercise.wwwapi/DataTransfer/Requests/CarPostRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace exercise.wwwapi.DataTransfer.Requests
+{
+    public class CarPostRequest
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+}
---
+++ exercise.wwwapi/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace exercise.wwwapi.Models
+{
+    [Table("cars")]
+    public class Car
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("make")]
+        public string Make { get; set; }
+
+        [Column("model")]
+        public string Model { get; set; }
+    }
+}
