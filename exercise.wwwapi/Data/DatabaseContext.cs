370a3f2a558ba7432bd2ebf70cd39ac8a73e6af7
agrmet <agron-metaj@hotmail.com>
2024-09-18 11:48:40 +0200

    Program.cs finished. Added models, repo, config, some code to context

---
+++ exercise.wwwapi/Data/DatabaseContext.cs
@@ -0,0 +1,11 @@
+﻿using exercise.wwwapi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace exercise.wwwapi.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
--- exercise.wwwapi/Program.cs
+++ exercise.wwwapi/Program.cs
@@ -1,9 +1,41 @@
+using exercise.wwwapi.Configuration;
+using exercise.wwwapi.Data;
+using exercise.wwwapi.Models;
+using exercise.wwwapi.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+var config = new ConfigurationSettings();
+
+builder.Services.AddScoped<IConfigurationSettings, ConfigurationSettings>();
+builder.Services.AddScoped<IDatabaseRepository<User>, DatabaseRepository<User>>();
+builder.Services.AddScoped<IDatabaseRepository<Post>, DatabaseRepository<Post>>();
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new()
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config.GetValue("AppSettings:Token"))),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateIssuerSigningKey = false
+    };
+});
+
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddDbContext<DatabaseContext>();
 
 var app = builder.Build();
 
@@ -14,7 +46,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true)
+                .AllowCredentials());
+
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers(); // Is this still necessary with minimal APIs?
 
 app.Run();
