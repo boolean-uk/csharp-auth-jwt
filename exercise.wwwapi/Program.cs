4ec4d810f4ddb2ef675a43fc2f25840fdd13a357
Malte <malte.stengard@gmail.com>
2024-09-18 11:38:19 +0200

    it works, authorization is go

--- exercise.wwwapi/Program.cs
+++ exercise.wwwapi/Program.cs
@@ -1,7 +1,92 @@
-var builder = WebApplication.CreateBuilder(args);
+using exercise.wwwapi.Configuration;
+using exercise.wwwapi.Endpoints;
+using exercise.wwwapi.Models;
+using exercise.wwwapi.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+var builder = WebApplication.CreateBuilder(args);
+var config = new ConfigurationSettings();
+
+
+builder.Services.AddScoped<IRepository<User>, Repository<User>>();
+builder.Services.AddScoped<IConfigurationSettings, ConfigurationSettings>();
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config.GetValue("AppSettings:Token"))),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = false,
+        ValidateIssuerSigningKey = false
+
+    };
+});
+builder.Services.AddSwaggerGen(s =>
+{
+    s.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "C# API Authentication",
+        Description = "Demo of an API using JWT as an authentication method",
+        Contact = new OpenApiContact
+        {
+            Name = "Nigel",
+            Email = "nigel@nigel.nigel",
+            Url = new Uri("https://www.boolean.co.uk")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Boolean",
+            Url = new Uri("https://github.com/boolean-uk/csharp-api-auth")
+        }
+
+    });
+
+    s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Add an Authorization header with a JWT token using the Bearer scheme see the app.http file for an example.)",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        In = ParameterLocation.Header,
+        Scheme = "Bearer"
+    });
+
+    s.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+
+});
+
+
+
+
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -16,5 +101,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.ConfigureUserEndpoints();
 
 app.Run();
