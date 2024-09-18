12782951857d4940572e269b9713234ffe9abed3
Victor <victor.chicinas@gmail.com>
2024-09-18 10:48:21 +0200

    Code skeleton created

--- exercise.wwwapi/Program.cs
+++ exercise.wwwapi/Program.cs
@@ -1,9 +1,20 @@
+using exercise.wwwapi.Configuration;
+using exercise.wwwapi.EndPoints;
+using exercise.wwwapi.Models;
+using exercise.wwwapi.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
+var config = new ConfigurationSettings();
+// Add services to the container.
+builder.Services.AddScoped<IConfigurationSettings, ConfigurationSettings>();
+builder.Services.AddScoped<IDatabaseRepository<User>, DatabaseRepository<User>>();
+
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -15,6 +26,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.MapControllers();
 
+app.ConfigureAuthApi();
 
 app.Run();
