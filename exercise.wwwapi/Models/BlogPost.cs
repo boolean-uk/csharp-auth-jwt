515236d78743417970314f7bb074e7381773e345
Julia <julia.r.lindgren@gmail.com>
2024-10-08 15:42:31 +0200

    Setup database

---
+++ exercise.wwwapi/Models/BlogPost.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace exercise.wwwapi.Models
+{
+    [Table("blogPost")]
+    public class BlogPost
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("text")]
+        public string Text { get; set; }
+
+        [ForeignKey("author")]
+        [Column("author_id")]
+        public int AuthorId { get; set; }
+        public User Author { get; set; }
+    }
+}
