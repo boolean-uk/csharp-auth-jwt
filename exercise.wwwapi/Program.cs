3e9aaca69cfe05c2b4c8ac86ec3cda194de7c636
Lowe Raivio <Lowe.Raivio@se.experis.com>
2025-01-30 12:45:48 +0100

    Added Authorization and Authentication to WebApplicationBuilder

--- exercise.wwwapi/Program.cs
+++ exercise.wwwapi/Program.cs
@@ -1,4 +1,8 @@
+using System.Text;
 using exercise.wwwapi.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +14,28 @@ builder.Services.AddSwaggerGen();
 // AddScoped
 builder.Services.AddScoped<IConfigurationSettings, ConfigurationSettings>();
 
+var conf = new ConfigurationSettings();
+builder.Services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer( x =>
+    {
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(conf.GetValue<string>("AppSettings:Token"))),
+            ValidateIssuer = false, 
+            ValidateAudience = false,
+            ValidateLifetime = false,
+            ValidateIssuerSigningKey = false
+        }; 
+    });
+
+builder.Services.AddAuthorization();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,7 +45,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true)
+    .AllowCredentials());
+
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 
 
 app.Run();
