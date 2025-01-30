acaeb2cce8be74422a0432d61f69c94702c6fcec
Lowe Raivio <Lowe.Raivio@se.experis.com>
2025-01-30 12:26:47 +0100

    Added Configuration settings

--- exercise.wwwapi/Program.cs
+++ exercise.wwwapi/Program.cs
@@ -1,9 +1,14 @@
+using exercise.wwwapi.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// AddScoped
+builder.Services.AddScoped<IConfigurationSettings, ConfigurationSettings>();
 
 var app = builder.Build();
 
