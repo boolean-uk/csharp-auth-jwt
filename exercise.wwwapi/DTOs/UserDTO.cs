a06795b1b2ed56c3a4ad5cf850343a84d2a963a7
GRehan <GRehan@N-NO-01-01-3380>
2025-01-31 08:38:51 +0100

    Finished core and started work on extension

---
+++ exercise.wwwapi/DTOs/UserDTO.cs
@@ -0,0 +1,13 @@
+﻿using exercise.wwwapi.Models;
+
+namespace exercise.wwwapi.DTOs
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string Email { get; set; }
+        public List<FollowerDTO> Followers { get; set; } = new List<FollowerDTO>();
+    }
+}
---
+++ exercise.wwwapi/Models/Post.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace exercise.wwwapi.Models
+{
+    [Table("posts")]
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column("text")]
+        public string Text { get; set; }
+
+        [ForeignKey("Author_Id")]
+        public int AuthorId { get; set; }
+        public User Author { get; set; }
+
+    }
+}
